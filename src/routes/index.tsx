0335cb016a5aef58551f31a4f9de6a37fad28dce
marcelo.azevedo <marcelo.azevedo@connectcorp.com.br>
2020-05-23 21:39:00 -0300

    Recuperação de senha

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,16 @@ import Route from './route';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
